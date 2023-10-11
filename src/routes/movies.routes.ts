396adbfdd4b96f3e76dd21372b8a92fc9253bf5f
sancho-sumy <www.sancho@gmail.com>
2023-10-11 23:27:55 +0300

    refactor: translate project to TypeScript
    * translate all server files and configurations to TypeScript
    * add necessary packages for TypeScript development
    * add types to all variables, parameters, and functions

--- src/routes/movies.routes.ts
+++ src/routes/movies.routes.ts
@@ -1,8 +1,8 @@
-import express from 'express';
+import express, { Router } from 'express';
 
-import { getMoviesHandler, getMovieHandler } from '../controllers/movies.controllers.js';
+import { getMoviesHandler, getMovieHandler } from '../controllers/movies.controllers';
 
-const router = express.Router();
+const router: Router = express.Router();
 
 /**
  * @openapi
